0843bb87cfe69846cd86eea6fa5c37866e60fbd3
Meggie <meggiecheng01@gmail.com>
2023-03-13 20:23:08 -0400

    fruits page

--- Fruit_lab/Fruits.js
+++ Fruit_lab/Fruits.js
@@ -6,3 +6,12 @@ var fruit = [
     { name: "Blueberry", quantity: 5, color: "blue" },
     { name: "Grapes", quantity: 10, color: "purple" }
 ];
+
+function draw() {
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(0, 0, 150, 100);
+}
+
+document.addEventListener('DOMContentLoaded', draw);
