59e163ae3ab9c22b0931ecdab2a7277f6c3385f8
Meggie <meggiecheng01@gmail.com>
2023-03-27 17:53:30 -0400

    json fruit lab

--- Fruit_lab/Fruits.js
+++ Fruit_lab/Fruits.js
@@ -10,8 +10,29 @@ var fruit = [
 function draw() {
     var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0, 0, 150, 100);
+    //size of each fruit bar
+    var section = Math.floor(canvas.height/fruit.length);
+    //position
+    var ypos = 0;
+
+    for (let i=0; i<fruit.length; i++) {
+        ctx.beginPath();
+        //bar colors
+        var fruitColor = fruit[i].color;
+        ctx.fillStyle = fruitColor;
+        ctx.fillRect(0, ypos, fruit[i].quantity*17, section);
+
+        //text
+        ctx.font = "17px serif"
+        ctx.fillStyle = "black";
+        //fruit quantity text
+        ctx.strokeText(fruit[i].quantity, 10, ypos+25);
+        //fruit name text
+        ctx.strokeText(fruit[i].name, 10, ypos+50);
+
+        //change position
+        ypos += section;
+    }
 }
 
 document.addEventListener('DOMContentLoaded', draw);
